3d8117d4e76649fb3a6ee063c08039b080894761
ApsaraSandamali <apsarasandamali321@gmail.com>
2021-11-27 19:45:29 +0530

    Update models

--- bankbackend/models/account.js
+++ bankbackend/models/account.js
@@ -15,6 +15,10 @@ const accountSchema = new Schema({
         type: String,
         required: true
     },
+    email : {
+        type: String,
+        required: true
+    },
     balance : {
         type: Number,
         required: true
