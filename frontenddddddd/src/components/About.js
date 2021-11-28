4e4fe3c7515c03b6fb5ab3b8eaece79ec9dab824
ApsaraSandamali <apsarasandamali321@gmail.com>
2021-11-28 16:11:19 +0530

    Implement transfer file

---
+++ frontenddddddd/src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./About.css";
+import currency from "../currency.png"
+
+export default function About() {
+    return (
+        <div className="bg_image">
+            
+        <p>
+            ABOUT US
+        </p>
+        </div>
+    )
+}
--- frontenddddddd/src/components/Transfer.js
+++ frontenddddddd/src/components/Transfer.js
@@ -19,7 +19,6 @@ export default function Transfer() {
                 } catch { }
             };
             Details();
-            
 
         }, [setAccount]);
     
