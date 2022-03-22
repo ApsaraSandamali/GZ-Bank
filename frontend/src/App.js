32ac1e9709a033a55e5b17a515d270102b3fee1a
apsi <apsarasandamali321@gmail.com>
2022-03-22 15:56:16 +0530

    update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,35 @@
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
 import About from './components/About';
-import AllStudents from './components/AllStudents';
 import Contact from './components/Contact';
-import EditStudent from './components/EditStudent';
 import Header from './components/Header';
 import Home from './components/Home';
 import Transfer from './components/Transfer';
 import TransferForm from './components/TransferForm';
+import EmailConfirm from './components/EmailConfirm';
+import TransferSuccess from './components/TransferSuccess';
+import Ceb from './components/ceb/Ceb';
+import CebConfirm from './components/ceb/CebConfirm';
+import CebAcc from './components/ceb/CebAcc';
+
+import Crib from './components/Crib';
 
 function App() {
   return (
     <Router>
       <div className="App">
         <Header />      
-        <Route path='/home' exact component={Home} />
+        <Route path='/' exact component={Home} />
         <Route path='/about' exact component={About} />
         <Route path='/contact' exact component={Contact} />
         <Route path='/transfer' exact component={Transfer} />
-        <Route path='/update/:id' exact component={EditStudent} />
-        <Route path='/get/:id' exact component={TransferForm} />
-        <Route path='/' exact component={AllStudents} />        
+        <Route path='/verifyEmail' exact component={EmailConfirm} />
+        <Route path='/email' exact component={TransferForm} />
+        <Route path='/success' exact component={TransferSuccess} />
+        <Route path='/crib' exact component={Crib} />
+        <Route path='/ceb' exact component={Ceb} />
+        <Route path='/cebconfirm' exact component={CebConfirm} />
+        <Route path='/cebacc' exact component={CebAcc} />
       </div>
     </Router>
 
